416dd6a47f0c0257af1f6ab617330363b6ed4dab
Alex Chamberlain <alexanderx.chamberlain@intel.com>
2015-10-14 18:47:46 -0700

    Make service start script read from atk.conf

--- bin/rest-server.sh
+++ bin/rest-server.sh
@@ -13,7 +13,8 @@ export YARN_CONF_DIR="/etc/hadoop/conf"
 # NOTE: Add this parameter to Java for connecting to a debugger
 # -agentlib:jdwp=transport=dt_socket,server=n,address=localhost:5005
 
-echo java $@ -XX:MaxPermSize=384m -cp "$LAUNCHER" org.trustedanalytics.atk.component.Boot rest-server
+echo java $@ -XX:MaxPermSize=384m -Dconfig.resource=atk.conf \
+     -cp "$LAUNCHER" org.trustedanalytics.atk.component.Boot rest-server
 java $@ -XX:MaxPermSize=384m -cp "$LAUNCHER" org.trustedanalytics.atk.component.Boot rest-server
 
 popd
